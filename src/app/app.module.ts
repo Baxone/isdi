1519ac286bd7ee7b31d230dd9ddb0d6245563fa2
Juanan <jantonio.perez@estudioclandestino.es>
2023-04-18 01:53:19 +0200

    creación de componentes, servicios e interfaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
+//libraries
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+//components
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/form/form.component';
+import { TasklistComponent } from './components/tasklist/tasklist.component';
+import { TaskComponent } from './components/task/task.component';
+import { FiltersComponent } from './components/filters/filters.component';
+import { AlertsComponent } from './components/alerts/alerts.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    TasklistComponent,
+    TaskComponent,
+    FiltersComponent,
+    AlertsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
